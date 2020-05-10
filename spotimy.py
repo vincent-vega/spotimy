e6b312eb4b30ce56e6275d6928e2884086856856
Davide Barelli <dbarelli@wersec.com>
2020-05-10 21:45:38 +0100

    refactoring

--- spotimy.py
+++ spotimy.py
@@ -2,9 +2,8 @@
 # -*- coding: utf-8 -*-
 
 from flask import Flask, render_template, redirect, request#, session
-from secrets import client_id, client_secret
+from secrets import client_id, client_secret, redirect_uri
 import base64
-import json
 import requests
 
 app = Flask('SpotiMy')
@@ -16,23 +15,31 @@ def root_endpoint():
 
 @app.route('/login', methods=['GET'])
 def login():
-    return redirect(f'https://accounts.spotify.com/authorize?client_id={client_id}&response_type=code&redirect_uri=http://localhost:5000/list&scope=playlist-read-private&state=123', code=302)
+    return redirect(f'https://accounts.spotify.com/authorize?client_id={client_id}&response_type=code&redirect_uri={redirect_uri}&scope=playlist-read-private', code=302)
 
-@app.route('/list', methods=['GET'])
-def list():
+@app.route('/spotifycb', methods=['GET'])
+def spotify_callback():
     code = request.args.get('code')
-    b64auth = base64.b64encode(bytes(f'{client_id}:{client_secret}', 'utf-8'))
+    b64auth = base64.b64encode(bytes(f'{client_id}:{client_secret}', 'utf-8')).decode('utf-8')
     resp = requests.post(f'https://accounts.spotify.com/api/token',
-                         data={'grant_type': 'authorization_code', 'code': code, 'redirect_uri': 'http://localhost:5000/list'},
-                         headers={'Authorization': f'Basic {base64.b64encode(bytes(f"{client_id}:{client_secret}", "utf-8")).decode("utf-8")}'})
+                         data={'grant_type': 'authorization_code', 'code': code, 'redirect_uri': redirect_uri},
+                         headers={'Authorization': f'Basic {b64auth}'})
+    # TODO error handling
     authdata = resp.json()
-    access_token = authdata['access_token']
-    resp = requests.get(f'https://api.spotify.com/v1/me/playlists', headers={'Authorization': f'Bearer {access_token}'})
-    data = resp.json()
-    print(data)
+    data = list_playlists(authdata['access_token'])
+    # TODO save access_token in a cookie
     if 'error' in data:
         return render_template('error.html', message=data['error']['message'])
-    return render_template('playlists.html', playlists=resp.json()['items'])
+    return render_template('playlists.html', playlists=data['items'])
+
+@app.route('/save/<playlist_id>', methods=['GET'])
+def save_playlist(playlist_id):
+    # TODO
+    pass
+
+def list_playlists(access_token: str) -> dict:
+    resp = requests.get(f'https://api.spotify.com/v1/me/playlists', headers={'Authorization': f'Bearer {access_token}'})
+    return resp.json() # TODO check response is json
 
 def main():
     app.config.update(
