38724c27994095600f8ae797058e490c10f1d774
Davide Barelli <dbarelli@wersec.com>
2020-05-11 21:53:34 +0100

    refactoring

---
+++ spotify/client.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+from spotify.secrets import client_id, client_secret, redirect_uri
+import base64
+import requests
+
+class Spotify:
+
+    @staticmethod
+    def get_authorize_uri() -> str:
+        return f'https://accounts.spotify.com/authorize?client_id={client_id}&response_type=code&redirect_uri={redirect_uri}&scope=playlist-read-private'
+
+    @staticmethod
+    def token(code: str) -> dict:
+        b64auth = base64.b64encode(bytes(f'{client_id}:{client_secret}', 'utf-8')).decode('utf-8')
+        resp = requests.post(f'https://accounts.spotify.com/api/token',
+                            data={'grant_type': 'authorization_code', 'code': code, 'redirect_uri': redirect_uri},
+                            headers={'Authorization': f'Basic {b64auth}'})
+        # TODO error handling
+        return resp.json()
+
+    @staticmethod
+    def list_playlists(access_token: str) -> dict:
+        resp = requests.get(f'https://api.spotify.com/v1/me/playlists',
+                            headers={'Authorization': f'Bearer {access_token}'})
+        return resp.json() # TODO check response is json
+
